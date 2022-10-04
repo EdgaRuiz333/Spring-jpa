91031ec384af8d4aba40313d95bd724734d89705
EdgaRuiz333 <estrada_rayados@hotmail.com>
2022-10-03 19:06:16 -0500

    first commit

---
+++ src/main/java/com/mdf/springjpa/Srping/jpa/SrpingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mdf.springjpa.Srping.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdf/springjpa/Srping/jpa/SrpingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdf.springjpa.Srping.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
